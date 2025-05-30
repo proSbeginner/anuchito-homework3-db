48dcec8005be1f7a7e7b21b718ee3a6806fae985
kittipong.p <kittipnog.p@arise.tech>
2025-05-30 19:26:01 +0700

    init

---
+++ src/main/java/com/anuchito/database/loan/LoanService.java
@@ -0,0 +1,31 @@
+package  com.anuchito.database.loan;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+
+    private final LoanRepository loanRepository;
+
+    public LoanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
+    public List<Loan> getAllLoans() {
+        return this.loanRepository.findAll();
+    }
+
+    public Optional<Loan> getLoanByLoanId(String loanId) {
+        return this.loanRepository.findByLoanId(loanId);
+    }
+
+    public Loan saveLoan(Loan loan) {
+        return this.loanRepository.save(loan);
+    }
+
+    public void deleteLoan(Long id) {
+        this.loanRepository.deleteById(id);
+    }
+}
